be6ab4089437631328d69546aa3947c660c10b2b
Rico Harsono <ricoharsono@gmail.com>
2026-03-05 15:14:34 +0800

    add picture and menu

--- igokochi/src/data/menu.js
+++ igokochi/src/data/menu.js
@@ -1,39 +1,70 @@
 // src/data/menu.js
+
+import dubaiMochi from "../assets/Dubai-Mochi-Chocolate.JPG";
+import earlGreyJasmineMatchaLatte from "../assets/Earl-Grey-X-Jasmine-Matcha-Latte.JPG";
+import mangoMatchaLatte from "../assets/Mango-Matcha-Latte.JPG";
+import matchaCoconutLatte from "../assets/Matcha-Coconut-Latte.JPG";
+import strawberryMatchaLatte from "../assets/Strawberry-Matcha-Latte.JPG";
+import earlGreyMatchaLatte from "../assets/Earl-Grey-Matcha-Latte.JPG";
+import matchaLatte from "../assets/Matcha-Latte.PNG";
+import kinakoCreamMatchaLatte from "../assets/Kinako-Cream-Matcha-Latte.PNG";
+
 const menu = [
   {
     id: "matcha-latte",
     name: "Matcha Latte",
-    description: "Stone-ground matcha with fresh milk.",
+    description: "Matcha Latte.",
     price: 5.5,
-    image: "https://picsum.photos/80?random=1",
+    image: matchaLatte,
   },
   {
-    id: "hojicha-latte",
-    name: "Hojicha Latte",
-    description: "Roasted green tea with a nutty aroma.",
+    id: "earl-grey-matcha-latte",
+    name: "Earl Grey Matcha Latte",
+    description: "Earl Grey Matcha Latte.",
     price: 5.0,
-    image: "https://picsum.photos/80?random=2",
+    image: earlGreyMatchaLatte,
   },
   {
-    id: "genmaicha",
-    name: "Genmaicha",
-    description: "Green tea blended with roasted rice.",
+    id: "earl-grey-x-jasmine-matcha-latte",
+    name: "Earl Grey X Jasmine Matcha Latte",
+    description: "Earl Grey Matcha Latte.",
+    price: 5.0,
+    image: earlGreyJasmineMatchaLatte,
+  },
+  {
+    id: "matcha-coconut-cloud",
+    name: "Matcha Coconut Cloud",
+    description: "Matcha Coconut Cloud.",
     price: 4.5,
-    image: "https://picsum.photos/80?random=3",
+    image: matchaCoconutLatte,
   },
   {
-    id: "matcha-sandwich",
-    name: "Matcha Sando",
+    id: "strawberry-matcha-latte",
+    name: "Strawberry Matcha Latte",
     description: "Soft bread with matcha cream filling.",
     price: 6.8,
-    image: "https://picsum.photos/80?random=4",
+    image: strawberryMatchaLatte,
   },
   {
-    id: "tamago-sando",
-    name: "Tamago Sando",
+    id: "mango-matcha-latte",
+    name: "Mango Matcha Latte",
     description: "Japanese egg sandwich with creamy texture.",
     price: 4.8,
-    image: "https://picsum.photos/80?random=5",
+    image: mangoMatchaLatte,
+  },
+  {
+    id: "kinako-cream-matcha-latte",
+    name: "Kinako Cream Matcha Latte",
+    description: "Japanese egg sandwich with creamy texture.",
+    price: 4.8,
+    image: kinakoCreamMatchaLatte,
+  },
+  {
+    id: "dubai-chewy-chocholate",
+    name: "Dubai Chewy Chocolate",
+    description: "Japanese egg sandwich with creamy texture.",
+    price: 4.8,
+    image: dubaiMochi,
   },
 ];
 
