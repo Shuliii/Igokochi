ecac0ac678d265c7d3c79c23ca1b377a67ba06e5
Rico Harsono <ricoharsono@gmail.com>
2026-04-28 16:52:54 +0800

    edit db

--- igokochi_backend/src/db.js
+++ igokochi_backend/src/db.js
@@ -2,9 +2,9 @@ import mysql from "mysql2/promise";
 console.log(process.env.DB_PORT);
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || "143.198.198.140",
-  port: process.env.DB_PORT || 30306,
-  user: process.env.DB_USER || "igokochi_user",
+  host: process.env.DB_HOST || "",
+  port: process.env.DB_PORT || "",
+  user: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "igokochi_pass",
   database: process.env.DB_NAME || "igokochi",
   waitForConnections: true,
